42d9a72f85c9ff524f9f7d303e14bec279af26cb
Vanshika00 <vanshika31032000@gmail.com>
2019-09-23 14:16:03 +0530

    first commit (added components)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { UserCartComponent } from './user-cart/user-cart.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavBarComponent,
+    ProductDetailsComponent,
+    ProductListComponent,
+    UserCartComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
