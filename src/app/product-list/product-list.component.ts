e4fe117873dc16b82e0cb6d4b1ebdfa6f2daf6a2
Vanshika00 <vanshika31032000@gmail.com>
2019-09-26 14:44:40 +0530

    fifth commit

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import {productItems} from './product.items';
 
 @Component({
   selector: 'app-product-list',
@@ -8,11 +9,13 @@ import {Router} from '@angular/router';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor(private router: Router) { }
+/*  constructor(private router: Router) { }*/
+  constructor() { }
+  products = productItems;
 
   ngOnInit() {
   }
-goToDetails(id) {
+/*goToDetails(id) {
   this.router.navigate(['/product-details'], {queryParams: {id: 1}});
-}
+}*/
 }
